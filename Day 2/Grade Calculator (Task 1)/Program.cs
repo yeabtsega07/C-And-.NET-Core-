ae1656db791fafc284ca909a939df4338310dfe6
Yeabtsega07 <yeabtse7@gmail.com>
2023-08-01 17:59:28 +0300

    Day 2 task

--- Day 2/Grade Calculator (Task 1)/Program.cs
+++ Day 2/Grade Calculator (Task 1)/Program.cs
@@ -9,22 +9,18 @@ namespace GradeCalculator
         {
             Console.WriteLine("Welcome to the Grade Calculator! 🙌 ");
 
-            // prompt user for name
             Console.Write("Please enter your Full Name: ");
             string fullName = Console.ReadLine();
 
-            // prompt user for number of courses
             Console.Write("Please enter the number of courses you have taken: ");
             int numberOfCourses = int.Parse(Console.ReadLine());
 
-            // prompt user for course names and grades
             Dictionary<string, int> courseGrades = new Dictionary<string, int>();
 
             for (int i = 0; i < numberOfCourses; i++)
             {
                 string courseName;
 
-                // prompt user for course name and validate that it is not a duplicate
                 do
                 {
                     Console.Write($"Please enter the name of course {i + 1}: ");
@@ -42,7 +38,6 @@ namespace GradeCalculator
 
                 int courseGrade;
 
-                // prompt user for course grade and validate that it is between 0 and 100
                 do
                 {
                     Console.Write($"Please enter the grade for {courseName}: ");
@@ -61,16 +56,8 @@ namespace GradeCalculator
                 courseGrades.Add(courseName, courseGrade);
             }
 
-            // calculate average grade
-            double totalGrade = 0;
-            foreach (var courseGrade in courseGrades)
-            {
-                totalGrade += courseGrade.Value;
-            }
+            double averageGrade = GetAverageGrade(courseGrades);
 
-            double averageGrade = totalGrade / numberOfCourses;
-
-            // display results
             Console.WriteLine($"Hello {fullName}! Your grades are: ");
 
             foreach (var courseGrade in courseGrades)
@@ -80,6 +67,19 @@ namespace GradeCalculator
 
             Console.WriteLine($"Hello {fullName}! Your average grade is {averageGrade}.");
 
+        }
+
+        static float GetAverageGrade(Dictionary<string, int> courseGrades)
+        {
+            float totalGrade = 0;
+            foreach (var courseGrade in courseGrades)
+            {
+                totalGrade += courseGrade.Value;
+            }
+
+            float averageGrade = totalGrade / courseGrades.Count;
+
+            return averageGrade;
         }
     }
 }
